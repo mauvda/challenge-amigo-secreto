21ec978359fe0c239ed487ac562dfbaf68c6206e
Mauricio Lamonatto <mauricio.lamonatto@gmail.com>
2025-01-23 21:44:08 -0300

    Adicionando funções de adicionarAmigo() e sortearAmigo() ao codigo de app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let cadastroNomes = [];
+
+function adicionarAmigo() {
+    let padrao = /[^a-zà-ú]/gi;
+    let validaCampo = document.querySelector('input').value;
+    validaCampo != '' && !validaCampo.match(padrao) ? 
+    cadastroNomes.push(validaCampo) : 
+    alert('Adicione um nome valido');
+    document.querySelector('input').value = '';
+    document.querySelector('input').placeholder = 'Digite um nome';
+    console.log(cadastroNomes);
+}
+
+function sortearAmigo() {
+    let amigoSorteado = Math.floor(Math.random() * cadastroNomes.length);
+    cadastroNomes.length > 1 ? 
+    document.getElementById('resultado').innerHTML = cadastroNomes[amigoSorteado] : 
+    alert("Antes do sorteio, adicione dois ou mais nomes");
+    
+}
